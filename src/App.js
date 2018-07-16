060b3e61571949f685a145471d559053ddc0c48a
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-16 12:58:27 -0700

    made a theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
+import { orange600, deepOrange600 } from 'material-ui/styles/colors';
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 import NavBar from './components/Navbar/Navbar'
 import Search from './components/Search/Search'
 import './App.css';
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: orange600,
+    accent1Color: deepOrange600,
+  },
+});
+
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div>
-          <NavBar />
-          <Search />
-        </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <NavBar />
+        <Search />
       </MuiThemeProvider>
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar';
 
-const Navbar = () => <AppBar title='Project 3'/>
+const Navbar = () => <AppBar title='Project 3' />
 
 export default Navbar;
